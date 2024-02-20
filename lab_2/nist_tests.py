8bab1bef9d588f52c4dfd0c01b0b5139827297fe
Вероника Будюк <veronika.budyuk2004@gmail.com>
2024-02-20 11:41:14 +0400

    make requirements and ditignore

--- lab_2/nist_tests.py
+++ lab_2/nist_tests.py
@@ -1,6 +1,8 @@
+import json
 import logging
 import math
-import json
+import os
+
 import scipy
 
 
@@ -8,39 +10,91 @@ logging.basicConfig(level=logging.INFO)
 
 
 class NistTest:
+    """
+    This class represents a NIST test on a given sequence of bits.
+
+    Attributes:
+    sequence : str
+        The sequence of bits.
+    len:int
+        sequence length
+
+    Methods:
+    bitwise_test(self) -> bool:
+        Performs the bitwise test on the sequence of bits.
+
+    same_bits_test(self) -> bool:
+        Performs the same bits test on the sequence of bits.
+
+    split_bits(self) -> list:
+        Splits the sequence of 8 bits into a list.
+
+    largest_number_of_units(self, blocks: list) -> dict:
+        Calculates the largest number of consecutive zeros or ones in the sequence.
+
+    length_test(self, dictionary: dict) -> bool:
+        Performs the length test on the sequence of bits using the largest number of units calculated.
+
+    """
+
     def __init__(self, bit_sequence: str) -> None:
+
         self.sequence = bit_sequence
         self.len = len(bit_sequence)
 
     def bitwise_test(self) -> bool:
-        sum = 0
-        for i in self.sequence:
-            if int(i) == 1:
-                sum += 1
-            else:
-                sum -= 1
-        sum *= 1 / math.sqrt(self.len)
-        p_value = math.erf(sum / math.sqrt(2))
-        return p_value > 0.01
+        """
+        Performs the bitwise test on the sequence of bits.
+
+        returns:
+        bool:true if the p-value is greater than 0.01, False otherwise.
+        """
+        try:
+            sum = 0
+            for i in self.sequence:
+                if int(i) == 1:
+                    sum += 1
+                else:
+                    sum -= 1
+            sum *= 1 / math.sqrt(self.len)
+            p_value = math.erf(sum / math.sqrt(2))
+            return p_value > 0.01
+        except Exception as ex:
+            logging.error(f"ZeroDivisionError: {ex.message}\n{ex.args}\n")
 
     def same_bits_test(self) -> bool:
-        counter = 0
-        for i in self.sequence:
-            counter += int(i)
-        counter *= 1 / self.len
-        if abs(counter - 0.5) < 2 / math.sqrt(self.len):
-            v = 0
-            for i in range(self.len - 1):
-                if float(i) != float(i + 1):
-                    v += 1
-            num = abs(v - 2 * self.len * counter * (1 - counter))
-            denom = 2 * math.sqrt(2 * self.len) * counter * (1 - counter)
-            p_value = math.erf(num / denom)
-        else:
-            p_value = 0
-        return p_value > 0.01
+        """
+        Performs the same bits test on the sequence of bits.
+
+        returns:
+        bool:true if the p-value is greater than 0.01, False otherwise.
+        """
+        try:
+            counter = 0
+            for i in self.sequence:
+                counter += int(i)
+            counter *= 1 / self.len
+            if abs(counter - 0.5) < 2 / math.sqrt(self.len):
+                v = 0
+                for i in range(self.len - 1):
+                    if float(i) != float(i + 1):
+                        v += 1
+                num = abs(v - 2 * self.len * counter * (1 - counter))
+                denom = 2 * math.sqrt(2 * self.len) * counter * (1 - counter)
+                p_value = math.erf(num / denom)
+            else:
+                p_value = 0
+            return p_value > 0.01
+        except Exception as ex:
+            logging.error(f"ZeroDivisionError: {ex.message}\n{ex.args}\n")
 
     def split_bits(self) -> list:
+        """
+        Splits the sequence of 8 bits into a list.
+
+        returns:
+        list:list of integers representing the split bits.
+        """
         blocks = []
         quantity = (self.len // 8) * 8
         for i in range(0, quantity, 8):
@@ -49,24 +103,47 @@ class NistTest:
         return blocks
 
     def largest_number_of_units(self, blocks: list) -> dict:
-        unit_counts = {}
-        for block in blocks:
-            counter = 0
-            max_counter = 0
-            for i in block:
-                if int(i) == 1:
-                    counter += 1
-                    max_counter = max(max_counter, counter)
+        """
+        Calculates the largest number of consecutive zeros or ones in the sequence.
+
+        parameters:
+        blocks: list of blocks of bits.
+
+        returns:
+        dict: dictionary containing the count of the largest units of consecutive zeros or ones.
+        """
+        try:
+            unit_counts = {}
+            for block in blocks:
+                counter = 0
+                max_counter = 0
+                for i in block:
+                    if int(i) == 1:
+                        counter += 1
+                        max_counter = max(max_counter, counter)
+                    else:
+                        counter = 0
+                if max_counter in unit_counts:
+                    unit_counts[max_counter] += 1
                 else:
-                    counter = 0
-            if max_counter in unit_counts:
-                unit_counts[max_counter] += 1
-            else:
-                unit_counts[max_counter] = 1
-        sorted_dict = dict(sorted(unit_counts.items(), key=lambda x: x[1]))
-        return sorted_dict
+                    unit_counts[max_counter] = 1
+            sorted_dict = dict(sorted(unit_counts.items(), key=lambda x: x[1]))
+            return sorted_dict
+        except Exception as ex:
+            logging.error(f"TypeError block wasn't str: {ex.message}\n{ex.args}\n")
 
     def length_test(self, dictionary: dict) -> bool:
+        """
+        Performs the length test on the sequence of bits
+        using the largest number of units calculated.
+
+        parameters:
+        dictionary: dictionary containing the count of the
+        largest units of consecutive zeros or ones.
+
+        returns:
+        bool:true if the p-value is greater than 0.01, False otherwise.
+        """
         try:
             pi = dict()
             pi[1] = 0.2148
@@ -81,16 +158,23 @@ class NistTest:
             return p_value > 0.01
         except Exception as ex:
             logging.error(
-                f"Length of the dictionary is greater than number of pi-constants: {ex.message}\n{ex.args}\n"
+                f"Length of the dictionary is longer than number of pi-constants: {ex.message}\n{ex.args}\n"
             )
 
 
 if __name__ == "__main__":
-    sequence = NistTest(
-        "011111011010010100000110101100111010011011100011100100001110111011100101101101111110010011101001011000101010100010111101000111100"
+
+    with open(os.path.join("lab_2", "sequence.json"), "r") as file:
+        seq = json.load(file)
+
+    sequence_java = NistTest(seq["java_sequence"])
+    sequence_java.bitwise_test()
+    sequence_java.same_bits_test()
+    sequence_java.length_test(
+        sequence_java.largest_number_of_units(sequence_java.split_bits())
     )
-    print(sequence.bitwise_test())
-    print(sequence.same_bits_test())
-    print(sequence.split_bits())
-    my_dict = sequence.largest_number_of_units(sequence.split_bits())
-    print(sequence.length_test(my_dict))
+
+    sequence_c = NistTest(seq["c_sequence"])
+    sequence_c.bitwise_test()
+    sequence_c.same_bits_test()
+    sequence_c.length_test(sequence_c.largest_number_of_units(sequence_c.split_bits()))
