804a5e52f4a1eae9b2021c2f0b178a17d6f312df
Вероника Будюк <veronika.budyuk2004@gmail.com>
2024-03-22 21:51:59 +0300

    done all point and desktop

--- lab_3/cryptography_part.py
+++ lab_3/cryptography_part.py
@@ -10,40 +10,61 @@ logging.basicConfig(level=logging.INFO)
 
 
 class Cryptograthy:
+    """Class of hybrid cryptosystem. Symmetric Triple Des encryption algorithm, Asymmetric RSA.
+    Methods:
+        1.key_generation(self)->None
+        2.encryption(self, text_file_path:str, encryption_file_path:str) -> None
+        3.decryption(self, encryption_file_path:str, decryption_file_path:str)->None:
+    """
 
-    def __init__(self, symmetric_key: str, private_key: str, public_key: str) -> None:
+    def __init__(
+        self, symmetric_key: str, private_key: str, public_key: str, key_size: int
+    ) -> None:
+        """parameters:
+        symmetric_key: the path by which to serialize the encrypted symmetric key;
+        public_key: the path by which to serialize the public key;
+        private_key: the path to serialize the private key;
+        key_size: the key size is at the user's choice (8, 16 or 24 bytes)"""
         self.symmetric_key = symmetric_key
         self.private_key = private_key
         self.public_key = public_key
-        self.key_size = 0
-
-    def key_generation(self, key_size):
-        # Генерация ключа для симметричного алгоритма
         self.key_size = key_size
-        symmetric_key = os.urandom(key_size)  # это байты
 
-        # Генерация ключа для ассимметричного алгоритма
+    def key_generation(self) -> None:
+        """The function which:
+        1.1. Generate a key for the symmetric algorithm.
+        1.2. Generate keys for the asymmetric algorithm.
+        1.3. Serialize asymmetric keys.
+        1.4. Encrypt the symmetric encryption key with a public key and save it in the specified path.
+        """
+        symmetric_key = os.urandom(self.key_size)
         private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
         public_key = private_key.public_key()
-
-        # сериализация открытого ключа в файл
-        with open(self.public_key, "wb") as public_out:
-            public_out.write(
-                public_key.public_bytes(
-                    encoding=serialization.Encoding.PEM,
-                    format=serialization.PublicFormat.SubjectPublicKeyInfo,
+        try:
+            with open(self.public_key, "wb") as public_out:
+                public_out.write(
+                    public_key.public_bytes(
+                        encoding=serialization.Encoding.PEM,
+                        format=serialization.PublicFormat.SubjectPublicKeyInfo,
+                    )
                 )
+        except Exception as ex:
+            logging.error(
+                f"Error serializing the public key to a file: {ex.message}\n{ex.args}\n"
             )
-        # сериализация закрытого ключа в файл
-        with open(self.private_key, "wb") as private_out:
-            private_out.write(
-                private_key.private_bytes(
-                    encoding=serialization.Encoding.PEM,
-                    format=serialization.PrivateFormat.TraditionalOpenSSL,
-                    encryption_algorithm=serialization.NoEncryption(),
+        try:
+            with open(self.private_key, "wb") as private_out:
+                private_out.write(
+                    private_key.private_bytes(
+                        encoding=serialization.Encoding.PEM,
+                        format=serialization.PrivateFormat.TraditionalOpenSSL,
+                        encryption_algorithm=serialization.NoEncryption(),
+                    )
                 )
+        except Exception as ex:
+            logging.error(
+                f"Error serializing the private key to a file: {ex.message}\n{ex.args}\n"
             )
-        # Шифрование симметричного ключа с помощью ассиметричного
         encrypted_symmetric_key = public_key.encrypt(
             symmetric_key,
             asymmetric.padding.OAEP(
@@ -52,15 +73,36 @@ class Cryptograthy:
                 label=None,
             ),
         )
-        # Сериализация зашифрованного ключа симметричного алгоритма в файл
-        with open(self.symmetric_key, "wb") as key_file:
-            key_file.write(encrypted_symmetric_key)
+        try:
+            with open(self.symmetric_key, "wb") as key_file:
+                key_file.write(encrypted_symmetric_key)
+        except Exception as ex:
+            logging.error(
+                f"Error serializing the symmetric key to a file: {ex.message}\n{ex.args}\n"
+            )
 
-    def encryption(self, text_file_path, encryption_file_path) -> None:
-        with open(self.symmetric_key, mode="rb") as key_file:
-            sym_key = key_file.read()
-        with open(self.private_key, "rb") as pem_in:
-            pr_key = pem_in.read()
+    def encryption(self, text_file_path: str, encryption_file_path: str) -> None:
+        """parameters:
+        text_file_path:the path to the encrypted text file;
+        encryption_file_path: the path to save the encrypted text file;
+        the function which:
+            2.1. Decrypt the symmetric key.
+            2.2. Encrypt the text using a symmetric algorithm and save it along the specified path.
+        """
+        try:
+            with open(self.symmetric_key, mode="rb") as key_file:
+                sym_key = key_file.read()
+        except Exception as ex:
+            logging.error(
+                f"Error deserelizations the symmetric key: {ex.message}\n{ex.args}\n"
+            )
+        try:
+            with open(self.private_key, "rb") as pem_in:
+                pr_key = pem_in.read()
+        except Exception as ex:
+            logging.error(
+                f"Error deserelizations the private key: {ex.message}\n{ex.args}\n"
+            )
         private_key = load_pem_private_key(pr_key, password=None)
         symmetric_key = private_key.decrypt(
             sym_key,
@@ -70,8 +112,11 @@ class Cryptograthy:
                 label=None,
             ),
         )
-        with open(text_file_path, "rb") as f:
-            data = f.read()
+        try:
+            with open(text_file_path, "rb") as f:
+                data = f.read()
+        except Exception as ex:
+            logging.error(f"File reading error: {ex.message}\n{ex.args}\n")
         padder = padding.PKCS7(self.key_size * 8).padder()
         padded_text = padder.update(data) + padder.finalize()
         iv = os.urandom(self.key_size)
@@ -79,14 +124,34 @@ class Cryptograthy:
         encryptor = cipher.encryptor()
         encrypted_data = encryptor.update(padded_text) + encryptor.finalize()
         encrypted_data = iv + encrypted_data
-        with open(encryption_file_path, "wb") as f:
-            f.write(encrypted_data)
+        try:
+            with open(encryption_file_path, "wb") as f:
+                f.write(encrypted_data)
+        except Exception as ex:
+            logging.error(f"File writing error: {ex.message}\n{ex.args}\n")
 
-    def decryption(self, encryption_file_path, decryption_file_path):
-        with open(self.symmetric_key, mode="rb") as key_file:
-            sym_key = key_file.read()
-        with open(self.private_key, "rb") as pem_in:
-            pr_key = pem_in.read()
+    def decryption(self, encryption_file_path: str, decryption_file_path: str) -> None:
+        """parameters:
+            encryption_file_path: the path to the encrypted text file;
+            decryption_file_path: the path to save the decrypted text file.
+        the function which:
+            3.1. Decrypt the symmetric key.
+            3.2. Decrypt the text using a symmetric algorithm and save it along the specified path.
+        """
+        try:
+            with open(self.symmetric_key, mode="rb") as key_file:
+                sym_key = key_file.read()
+        except Exception as ex:
+            logging.error(
+                f"Error deserelizations the symmetric key: {ex.message}\n{ex.args}\n"
+            )
+        try:
+            with open(self.private_key, "rb") as pem_in:
+                pr_key = pem_in.read()
+        except Exception as ex:
+            logging.error(
+                f"Error deserelizations the private key: {ex.message}\n{ex.args}\n"
+            )
         private_key = load_pem_private_key(pr_key, password=None)
         symmetric_key = private_key.decrypt(
             sym_key,
@@ -96,8 +161,11 @@ class Cryptograthy:
                 label=None,
             ),
         )
-        with open(encryption_file_path, "rb") as f:
-            encrypted_data = f.read()
+        try:
+            with open(encryption_file_path, "rb") as f:
+                encrypted_data = f.read()
+        except Exception as ex:
+            logging.error(f"File reading error: {ex.message}\n{ex.args}\n")
         iv = encrypted_data[: self.key_size]
         encrypted_data = encrypted_data[self.key_size :]
         cipher = Cipher(algorithms.TripleDES(symmetric_key), modes.CBC(iv))
@@ -105,19 +173,8 @@ class Cryptograthy:
         data = decryptor.update(encrypted_data) + decryptor.finalize()
         unpadder = padding.PKCS7(self.key_size * 8).unpadder()
         data = unpadder.update(data) + unpadder.finalize()
-        # Сохранение расшифрованных данных в файл
         try:
             with open(decryption_file_path, "wb") as f:
                 f.write(data)
         except Exception as e:
-            logging.error(e)
-
-
-if __name__ == "__main__":
-
-    my_key = Cryptograthy(
-        "lab_3\symmetric.txt", "lab_3\private.pem", "lab_3\public.pem"
-    )
-    my_key.key_generation(8)
-    my_key.encryption("lab_3/base_text.txt", "encryption_text.txt")
-    my_key.decryption("encryption_text.txt", "decryption_text.txt")
+            logging.error(f"File writing error: {ex.message}\n{ex.args}\n")
